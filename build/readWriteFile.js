69d9781c2bddd5ae0deb23ebf68c42647e8dcd86
Neema-sheby <73499706+Neema-sheby@users.noreply.github.com>
2023-01-20 16:30:26 +0000

    changes to file path of read/write in readWriteFile.ts

--- build/readWriteFile.js
+++ build/readWriteFile.js
@@ -5,60 +5,60 @@ const fs = require("fs");
 //-------------------------------------------------------------------
 // Write the plateau Grid
 function writePlateauGrid(content) {
-    fs.writeFileSync(`${__dirname}/files/plateauGrid.txt`, content);
+    fs.writeFileSync(`${__dirname}/plateauGrid.txt`, content);
 }
 exports.writePlateauGrid = writePlateauGrid;
 // Read the plateau Grid
 function readPlateauGrid() {
-    const textRead = fs.readFileSync(`${__dirname}/files/plateauGrid.txt`, "utf-8");
+    const textRead = fs.readFileSync(`${__dirname}/plateauGrid.txt`, "utf-8");
     return textRead;
 }
 exports.readPlateauGrid = readPlateauGrid;
 //-------------------------------------------------------------------
 // write if plateau grid is set
 function writeIsPlateauSet(content) {
-    fs.writeFileSync(`${__dirname}/files/isPlateauSet.txt`, content);
+    fs.writeFileSync(`${__dirname}/isPlateauSet.txt`, content);
 }
 exports.writeIsPlateauSet = writeIsPlateauSet;
 // Read if plateau grid is set
 function ReadIsPlateauSet() {
-    const textRead = fs.readFileSync(`${__dirname}/files/isPlateauSet.txt`, "utf-8");
+    const textRead = fs.readFileSync(`${__dirname}/isPlateauSet.txt`, "utf-8");
     return textRead;
 }
 exports.ReadIsPlateauSet = ReadIsPlateauSet;
 //-------------------------------------------------------------------
 // Write the selected Rover
 function writeSelectedRoverName(content) {
-    fs.writeFileSync(`${__dirname}/files/selectedRover.txt`, content);
+    fs.writeFileSync(`${__dirname}/selectedRover.txt`, content);
 }
 exports.writeSelectedRoverName = writeSelectedRoverName;
 // Read the selected Rover
 function readSelectedRoverName() {
-    const textRead = fs.readFileSync(`${__dirname}/files/selectedRover.txt`, "utf-8");
+    const textRead = fs.readFileSync(`${__dirname}/selectedRover.txt`, "utf-8");
     return textRead;
 }
 exports.readSelectedRoverName = readSelectedRoverName;
 //-------------------------------------------------------------------
 //Write the inputed Rover location
 function writeRoverLocation(content) {
-    fs.writeFileSync(`${__dirname}/files/roverLocation.txt`, content);
+    fs.writeFileSync(`${__dirname}/roverLocation.txt`, content);
 }
 exports.writeRoverLocation = writeRoverLocation;
 // Read the inputed Rover Location
 function readRoverLocation() {
-    const textRead = fs.readFileSync(`${__dirname}/files/roverLocation.txt`, "utf-8");
+    const textRead = fs.readFileSync(`${__dirname}/roverLocation.txt`, "utf-8");
     return textRead;
 }
 exports.readRoverLocation = readRoverLocation;
 //-------------------------------------------------------------------
 // Write (true/false) if Rover is selected
 function writeIsRoverSelected(content) {
-    fs.writeFileSync(`${__dirname}/files/isRoverSelected.txt`, content);
+    fs.writeFileSync(`${__dirname}/isRoverSelected.txt`, content);
 }
 exports.writeIsRoverSelected = writeIsRoverSelected;
 // Read if Rover is selected
 function readIsRoverSelected() {
-    const textRead = fs.readFileSync(`${__dirname}/files/isRoverSelected.txt`, "utf-8");
+    const textRead = fs.readFileSync(`${__dirname}/isRoverSelected.txt`, "utf-8");
     return textRead;
 }
 exports.readIsRoverSelected = readIsRoverSelected;
--- src/readWriteFile.ts
+++ src/readWriteFile.ts
@@ -4,15 +4,12 @@ const fs = require("fs");
 
 // Write the plateau Grid
 export function writePlateauGrid(content: string): void {
-  fs.writeFileSync(`${__dirname}/files/plateauGrid.txt`, content);
+  fs.writeFileSync(`${__dirname}/plateauGrid.txt`, content);
 }
 
 // Read the plateau Grid
 export function readPlateauGrid(): string {
-  const textRead = fs.readFileSync(
-    `${__dirname}/files/plateauGrid.txt`,
-    "utf-8"
-  );
+  const textRead = fs.readFileSync(`${__dirname}/plateauGrid.txt`, "utf-8");
   return textRead;
 }
 
@@ -20,15 +17,12 @@ export function readPlateauGrid(): string {
 
 // write if plateau grid is set
 export function writeIsPlateauSet(content: string): void {
-  fs.writeFileSync(`${__dirname}/files/isPlateauSet.txt`, content);
+  fs.writeFileSync(`${__dirname}/isPlateauSet.txt`, content);
 }
 
 // Read if plateau grid is set
 export function ReadIsPlateauSet(): string {
-  const textRead = fs.readFileSync(
-    `${__dirname}/files/isPlateauSet.txt`,
-    "utf-8"
-  );
+  const textRead = fs.readFileSync(`${__dirname}/isPlateauSet.txt`, "utf-8");
   return textRead;
 }
 
@@ -36,15 +30,12 @@ export function ReadIsPlateauSet(): string {
 
 // Write the selected Rover
 export function writeSelectedRoverName(content: string): void {
-  fs.writeFileSync(`${__dirname}/files/selectedRover.txt`, content);
+  fs.writeFileSync(`${__dirname}/selectedRover.txt`, content);
 }
 
 // Read the selected Rover
 export function readSelectedRoverName(): string {
-  const textRead = fs.readFileSync(
-    `${__dirname}/files/selectedRover.txt`,
-    "utf-8"
-  );
+  const textRead = fs.readFileSync(`${__dirname}/selectedRover.txt`, "utf-8");
   return textRead;
 }
 
@@ -52,15 +43,12 @@ export function readSelectedRoverName(): string {
 
 //Write the inputed Rover location
 export function writeRoverLocation(content: string): void {
-  fs.writeFileSync(`${__dirname}/files/roverLocation.txt`, content);
+  fs.writeFileSync(`${__dirname}/roverLocation.txt`, content);
 }
 
 // Read the inputed Rover Location
 export function readRoverLocation(): string {
-  const textRead = fs.readFileSync(
-    `${__dirname}/files/roverLocation.txt`,
-    "utf-8"
-  );
+  const textRead = fs.readFileSync(`${__dirname}/roverLocation.txt`, "utf-8");
   return textRead;
 }
 
@@ -68,14 +56,11 @@ export function readRoverLocation(): string {
 
 // Write (true/false) if Rover is selected
 export function writeIsRoverSelected(content: string): void {
-  fs.writeFileSync(`${__dirname}/files/isRoverSelected.txt`, content);
+  fs.writeFileSync(`${__dirname}/isRoverSelected.txt`, content);
 }
 
 // Read if Rover is selected
 export function readIsRoverSelected(): string {
-  const textRead = fs.readFileSync(
-    `${__dirname}/files/isRoverSelected.txt`,
-    "utf-8"
-  );
+  const textRead = fs.readFileSync(`${__dirname}/isRoverSelected.txt`, "utf-8");
   return textRead;
 }
